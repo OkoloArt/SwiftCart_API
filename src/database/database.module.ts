6d56496d2559cfa55adba3ff340cc9654f625fba
Arthur Okolo <Okoloarthur4@gmail.com>
2024-02-14 20:58:43 +0100

    Initialize product resources and postgres database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User],
+        ssl: {
+          rejectUnauthorized: false, // Set to true in production for a secure connection
+        },
+        connection: {
+          options: `project=${configService.get('ENDPOINT_ID')}`,
+        },
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
