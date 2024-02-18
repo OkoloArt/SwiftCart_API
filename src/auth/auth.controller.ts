229cbbc75651968d09f1999c9995b532dc755001
Arthur Okolo <Okoloarthur4@gmail.com>
2024-02-18 19:29:10 +0100

    authentication logic to register a new user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,17 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDto } from 'src/user/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('signup')
-  signUp(@Body(ValidationPipe) createUserDto: UserDto) {
-    return this.authService.signUp(createUserDto);
+  @Post('register')
+  register(@Body(ValidationPipe) userDto: UserDto) {
+    return this.authService.register(userDto);
   }
 
   @Post('login')
-  login(@Body(ValidationPipe) createUserDto: UserDto) {}
+  login(@Body(ValidationPipe) loginDto: LoginDto) {}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { UserDto } from 'src/user/dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
 
-  async signUp(createUserDto: UserDto) {
-    const { password, ...rest } = createUserDto;
+  async register(userDto: UserDto) {
+    const { password, ...rest } = userDto;
 
     // Hash the password using bcrypt
     const hashPassword = await bcrypt.hash(password, 10);
@@ -17,5 +18,5 @@ export class AuthService {
     return user;
   }
 
-  login() {}
+  async login(loginDto: LoginDto) {}
 }
