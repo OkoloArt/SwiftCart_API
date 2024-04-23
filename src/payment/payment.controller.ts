a4d1c6ed2443c15b18532c324c8a660fd84d45e8
Arthur Okolo <Okoloarthur4@gmail.com>
2024-04-23 15:55:03 +0100

    add AuthGuard to payment endpoints

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,15 +1,14 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
   Param,
-  Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentDto, SubscriptionDto } from '../libs/dto/payment.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
 
 @ApiTags('Payment Manager')
 @Controller('payment')
@@ -17,11 +16,15 @@ export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
   @Post('product')
+  @ApiBearerAuth('Bearer')
+  @UseGuards(JwtAuthGuard)
   productPayment(@Body() createPaymentDto: PaymentDto) {
     return this.paymentService.productPayment(createPaymentDto);
   }
 
   @Post(':id/subscribe-seller')
+  @ApiBearerAuth('Bearer')
+  @UseGuards(JwtAuthGuard)
   subscribeSeller(
     @Param('id') userId: string,
     @Body() subscriptionDto: SubscriptionDto,
@@ -30,6 +33,8 @@ export class PaymentController {
   }
 
   @Post(':id/cancel-subscription/:shouldCancel')
+  @ApiBearerAuth('Bearer')
+  @UseGuards(JwtAuthGuard)
   cancelSubscription(
     @Param('id') userId: string,
     @Param('shouldCancel') shouldCancel: boolean,
